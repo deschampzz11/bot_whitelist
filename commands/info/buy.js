aac61acac59c2202c758de8bc9c8e7bf6240b6e5
poori99988 <rapeepat0312@gmail.com>
2021-12-22 19:58:27 +0700

    Update buy.js

--- commands/info/buy.js
+++ commands/info/buy.js
@@ -15,32 +15,27 @@ module.exports = {
     await message.delete();
 
 dbwhitelist.findOne({ userID: message.author.id }, async (err, user) => {
-
-    let errurl = new MessageEmbed()
-    .setTitle('ไม่พบลิ้งอังเปา')
-    .setDescription("```diff\n- กรุณาใส้ลิ้งซองด้วยน้า```")
-    .setThumbnail("https://images-ext-1.discordapp.net/external/nCLyCoFKrSceZiXoAnWsnycEYmL3bqD24rWs-7IZPEM/https/i.vgy.me/ss9v5e.gif")
-    .setColor('RED')
-
-    let errnum = new MessageEmbed()
-    .setTitle('เลขอังเปาไม่ถูกต้อง')
-    .setDescription(`\`\`\`diff\n- ${message.author.username} กรุณาลองอีกครั้งน้า\`\`\``)
-    .setColor('RED')
-
-let tw_gif = args[0]
+  let errurl = new MessageEmbed()
+  .setTitle('ไม่พบลิ้งอังเปา')
+  .setDescription("```diff\n- กรุณาใส้ลิ้งซองด้วยน้า```")
+  .setThumbnail("https://images-ext-1.discordapp.net/external/nCLyCoFKrSceZiXoAnWsnycEYmL3bqD24rWs-7IZPEM/https/i.vgy.me/ss9v5e.gif")
+  .setColor('RED')
+  
+  let errnum = new MessageEmbed()
+  .setTitle('เลขอังเปาไม่ถูกต้อง')
+  .setDescription(`\`\`\`diff\n- ${message.author.username} กรุณาลองอีกครั้งน้า\`\`\``)
+  .setColor('RED')
+  let tw_gif = args[0]
   if(!tw_gif){
     message.channel.send({ embeds: [errurl] })
   }
   if(tw_gif){
-
+    
     truewallet.redeemvouchers(client.config.wallet, tw_gif)
-
+    
     .then(async (res) => {
-
       if(res.amount === null || res.amount === undefined){
-
-          message.channel.send({ embeds: [errnum] })
-
+        message.channel.send({ embeds: [errnum] })
       }else{
         if(!whitelist) {
             const newwhitelist = new dbwhitelist({
