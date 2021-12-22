8dc143d18537f0e5eca8a2534905bd1ab3f4e8b4
poori99988 <rapeepat0312@gmail.com>
2021-12-23 00:53:47 +0700

    Update buy.js

--- commands/info/buy.js
+++ commands/info/buy.js
@@ -18,7 +18,7 @@ module.exports = {
 dbwhitelist.findOne({ userID: message.author.id }, async (err, user) => {
 
   if(whitelist) {
-    message.channel.send({ embeds: [new MessageEmbed().setDescription(`[ ! ] ${message.author.name} มีไวริสแล้ว`).setColor('RED')] })
+    return message.channel.send({ embeds: [new MessageEmbed().setDescription(`[ ! ] ${message.author.name} มีไวริสแล้ว`).setColor('RED')] })
   }
 
   let errurl = new MessageEmbed()
